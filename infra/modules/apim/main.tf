4c3e3023d71f8b3f3d2d0fbbddb6c5d8a72ccbb3
jihys <jihyeseo@microsoft.com>
2026-06-13 06:00:41 +0000

    Fix emit-metric value: use Convert.ToDouble for numeric value
    APIM emit-metric 'value' attribute requires a numeric type.
    String cast caused 400 ValidationError. Wrapped with Convert.ToDouble().

--- infra/modules/apim/main.tf
+++ infra/modules/apim/main.tf
@@ -239,7 +239,7 @@ resource "azurerm_api_management_product_policy" "project" {
             <set-variable name="completionTokens" value="@{var body = (JObject)context.Variables[&quot;responseBody&quot;]; return body[&quot;usage&quot;]?[&quot;completion_tokens&quot;]?.ToString() ?? &quot;0&quot;;}" />
             <set-variable name="totalTokens" value="@{var body = (JObject)context.Variables[&quot;responseBody&quot;]; return body[&quot;usage&quot;]?[&quot;total_tokens&quot;]?.ToString() ?? &quot;0&quot;;}" />
             <set-variable name="modelName" value="@{var body = (JObject)context.Variables[&quot;responseBody&quot;]; return body[&quot;model&quot;]?.ToString() ?? &quot;unknown&quot;;}" />
-            <emit-metric name="TokenUsage" value="@((string)context.Variables[&quot;totalTokens&quot;])" namespace="apim-foundry">
+            <emit-metric name="TokenUsage" value="@(Convert.ToDouble((string)context.Variables[&quot;totalTokens&quot;]))" namespace="apim-foundry">
               <dimension name="subscription-id" value="@(context.Subscription.Id)" />
               <dimension name="subscriber" value="@(context.User.Email ?? context.User.Id)" />
               <dimension name="project-name" value="@(context.Product.Name)" />
